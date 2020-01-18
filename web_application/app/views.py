336fa22626b23028ee3e475bd0448f5c22aee209
Martin Oywa <martinoywa2@gmail.com>
2020-01-18 15:19:38 +0300

    add upload funtionality

--- web_application/app/views.py
+++ web_application/app/views.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def home():
-	return render_template('index.html')
+	if request.method == 'GET':
+		return render_template('index.html')
+	else:
+		print(request.files)
+
+		if 'image' not in request.files:
+			print('No file uploaded')
+
+		image = request.files['image'].read()
+
+		return render_template('result.html', image=image)
