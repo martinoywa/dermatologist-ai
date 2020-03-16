91811b410d0aac9424b92ee3216afe1d520cd459
Martin Oywa <martinoywa2@gmail.com>
2020-03-16 23:34:34 +0300

    Rename folders

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .views import main
+from .api import main
 
 
 app = Flask(__name__)
--- app/app/model/inference.py
+++ app/app/model/inference.py
@@ -1,6 +1,7 @@
 import io
 
 import torch
+import torch.nn as nn
 from torchvision import transforms, models
 
 from PIL import Image
@@ -14,14 +15,15 @@ transform = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
 ])
 
-checkpoint = Path('app/model/skin_cancer_model.pt')
-model = models.densenet201(pretrained=True)
+checkpoint = Path('app/model/checkpoint.pt')
+model = models.vgg16(pretrained=True)
+
+model.features = nn.Sequential(*list(model.features.children())[:-3])
+model.classifier = nn.Sequential(*[nn.Linear(25088, 4096), nn.ReLU(inplace=True),
+                                   nn.Dropout(p=.5), nn.Linear(4096, 3)])
 	
 for param in model.features.parameters():
 	param.requires_grad = False
-    
-in_feat = model.classifier.in_features
-model.classifier = torch.nn.Linear(in_feat, 3)
 	
 model.load_state_dict(torch.load(checkpoint, map_location='cpu'))
 
